8306360f5d2508ffff5c3a5cb36c47cc8c82dd16
ayato_p <ayato.skycrawlers@gmail.com>
2015-12-09 12:10:52 +0900

    Add tower-of-hanoi proj

---
+++ tower-of-hanoi/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to animation-toh-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ tower-of-hanoi/test/animation_toh_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns animation-toh-clj.core-test
+  (:require [clojure.test :refer :all]
+            [animation-toh-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
